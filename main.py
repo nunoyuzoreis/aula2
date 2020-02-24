81b52fca7c019310789016453dde4207e6a4a205
Nuno Reis <nunoyuzoreis@gmail.com>
2020-02-24 18:53:00 -0300

    colocando fatorial denymtro de uma funcao

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-print("insira um numero")
-num = int(input())
-cont = 2
-fatorial = 1
-while cont <= num:
-	fatorial = fatorial * cont
-	cont = cont +1
+def fatorial():
+	print("insira um numero")
+	num = int(input())
+	cont = 2
+	fatorial = 1
+	while cont <= num:
+		fatorial = fatorial * cont
+		cont = cont +1
 	
-print(fatorial)
+	print(fatorial)
+
+
+fatorial()
